7b6dfabd7e7024f6c9b8bb5938fc8b72c8e17780
Luiz <valuiz1@gmail.com>
2025-05-01 17:55:39 -0300

    Created engineer landing page

--- engineer_page/views.py
+++ engineer_page/views.py
@@ -5,4 +5,5 @@ from django.http import HttpRequest
 def index(r: HttpRequest):
     with open("eng_description.txt", "r") as description_file:
         description = description_file.read()
-    return render(r, "eng_index.html", {"eng_description": description})
+        paragraphs = description.split("\n")
+    return render(r, "eng_index.html", {"paragraphs": paragraphs})

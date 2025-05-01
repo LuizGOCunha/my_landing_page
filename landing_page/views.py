f8f92c5b1be652da184dbadd0348e2cb4247f6bc
Luiz <valuiz1@gmail.com>
2025-05-01 13:28:59 -0300

    Added basic index

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,9 +1,10 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
 
 
-def index(r: WSGIRequest):
-    return HttpResponse("<h1>This is the landing page</h1>")
+def index(r: HttpRequest):
+    with open("eng_description.txt", "r") as description_file:
+        description = description_file.read()
+    return render(r, "landing_page/index.html", {"eng_description": description})
